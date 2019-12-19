4a402154fb37eb9d60af12eb2ad975ba04245a8a
arzek357 <arzek357@gmail.com>
2019-12-19 09:56:02 +0300

    Initial commit

---
+++ android/src/com/mystargame/arzek/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mystargame.arzek;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mystargame.arzek.StarGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StarGame(), config);
+	}
+}
---
+++ desktop/src/com/mystargame/arzek/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mystargame.arzek.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mystargame.arzek.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
